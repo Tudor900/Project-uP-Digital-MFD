d2c3f6971f5dcca16b30f81343c044fd9d3d9479
Tudor900 <tudor900f1@gmail.com>
2024-12-10 22:08:26 +0200

    changed name

--- interface.py
+++ interface.py
@@ -3,23 +3,19 @@ from threading import Thread
 import time
 import random
 
+
 app = Flask(__name__)
 label_text = "Original Label"
-sensor = 200
-def sensor_test_value():
-    while(True):
-        sensor = random.randrange(0,200)
-        time.sleep(2)
-        print(sensor)
-        return sensor
+
+  
+
+
+
 # Background task to update the label
 def modify_speed():
-    global label_text
-    global sensor
-    while True:
-        
+    while True:      
           # Simulate a long-running task
-        label_text = sensor_test_value()
+        label_text = "test"
 
 @app.route('/')
 def index():
--- obdconnection.py
+++ obdconnection.py
@@ -3,30 +3,39 @@ import time
 from obd import OBDCommand, Unit
 from obd.protocols import ECU
 from obd.utils import bytes_to_int
-obd.logger.setLevel(obd.logging.DEBUG)
+#obd.logger.setLevel(obd.logging.DEBUG)
 # OBD connection parameters
 fuel_used = 0
 fuel_price = 7.11  # Price per liter in RON
 total_cost = 0
+def test():
+    return "Main is connected"
+    exit(1)
+
+
+
 connection = obd.Async(portstr="/dev/rfcomm0", protocol="6", baudrate=115200, fast=False, timeout=30)
 
 print("Connection Status:", connection.is_connected())
+
 
 
 def printRPM(r):
     global RPM
     RPM = r.value.magnitude
     print(RPM)
+    return RPM
     #time.sleep(1)
     
 def printSPEED(r):
     global SPEED
     SPEED = r.value.magnitude
     print(SPEED)
+    return SPEED
     #time.sleep(1)
 
     
-connection.watch(obd.commands.MAF, callback=printMAF)
+connection.watch(obd.commands.MAF, callback=printRPM)
 connection.watch(obd.commands.SPEED, callback=printSPEED)
 
 connection.start()
